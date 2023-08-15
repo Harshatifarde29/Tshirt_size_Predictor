272987d04c55a42878cfbfdf447e7cf8693c39d3
Harshatifarde29 <92580014+Harshatifarde29@users.noreply.github.com>
2023-08-15 21:06:40 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,39 @@
+#import lib
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+
+#load the data
+data = pd.read_csv("tshirt_data_m23.csv")
+print(data)
+
+#check for null data
+print(data.isnull().sum())
+
+#feature and target
+features=data[["Height(cm)","Weight(kg)"]]
+target=data["T-Shirt Size"]
+print(features)
+print(target)
+
+#feature scaling
+mms=MinMaxScaler()
+nfeatures=mms.fit_transform(features)
+print(nfeatures)
+
+#model
+model=KNeighborsClassifier(n_neighbors=3,metric="euclidean")
+model.fit(nfeatures,target)
+
+#predict
+height=float(input("enter height"))
+weight=float(input("enter weight"))
+data=[[height,weight]]
+ndata=mms.transform(data)
+
+size=model.predict(ndata)
+print(size)
+
+#internal working
+nn=model.kneighbors(ndata,n_neighbors=3)
+print(nn)
